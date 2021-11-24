663475b2aa2dbcf7b3e895639148c8914e8cdd2d
skillboost <skillboost@protonmail.com>
2021-11-24 11:29:42 +0400

    FIX: GitHub Actions Tests hangs error: CI=true flax in command

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// afterEach(() => {
+//   cleanup()
+// });
+
+// afterAll(() => {
+//   cleanup();
+// });
+
 test('renders learn react link', () => {
   // render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
